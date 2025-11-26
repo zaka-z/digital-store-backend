61332e059af8c5ae6a9e91ffe4cf17752b5025ed
zaka-z <zachariyatech@gmail.com>
2025-11-26 12:29:01 +0330

    آپدیت فایل‌های جدید

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: '*' }));
 
 // اتصال به دیتابیس
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log('✅ MongoDB Connected'))
   .catch(err => console.error('❌ DB Error:', err));
 
